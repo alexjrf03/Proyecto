42a250e81306f06d04fcfb2e404ca673216ef76b
Abraham15rivas <abraham15rivas@gmail.com>
2020-04-29 01:29:48 -0430

    changes

--- View/Module/report/print_view.php
+++ View/Module/report/print_view.php
@@ -4,21 +4,29 @@
         <title>Reporte de Aplicación</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <style>
-
-            * {
-                font-size: 10px;
+        <style>            
+            #container {
+                width: 100vh;
             }
-
-            .titulo{
+            .width {
+                width: 1000px !important
+            }
+            .th {
+                background-color: #354757;
+                color: #fff;
+                text-align:center;
+            }
+            table {
+                font-size: 15px
+            }
+            .nro_comprobante {
+                text-align: right;
+                color: red;
+                font-weight: bold
+            }
+            h3 {
                 color: blue;
             }
-
-            #container {
-                width: 500px;
-            }
-           
         </style>
 
     </head>
@@ -26,13 +34,61 @@
 
         <div id="container">
 
-            <div class="padding">
-                <!-- <img src="../../img/barra.png"/> -->
+            <div class="barra">
+                <img src="../../img/barra.png" height="30px"/>
             </div>
 
             <?php
 
-                print_r($app);
+                $nro_comprobante = rand(10,4);
+                
+                echo '
+                <p class="nro_comprobante">N° Comprobante: '.$nro_comprobante.'</p>
+                <h3 style="text-align: center;">Sistema de Gestión de Inventario de Aplicaciones</h3>
+                
+                <h4>Datos de la Aplicación:</h4>
+            
+                <table class="width" border="1" style="padding:5px 10px;">
+                    
+                    <tr>
+                        <th class="th">Nombre</th>
+                        <th class="th">Usuario Final</th>
+                        <th class="th">URL</th>
+                        <th class="th">Uso</th>
+                    </tr>
+                        
+                    <tr>
+                        <td>'.$app['name'].'</td>
+                        <td>'.$app['final_user'].'</td>
+                        <td>'.$app['url'].'</td>
+                        <td>'.$app['uso'].'</td>
+                    </tr> 
+
+                    <tr>
+                        <th class="th">Archivo Conexión</th>
+                        <th class="th">Manejo de Firmas</th>
+                        <th class="th">Archivos Adjuntos</th>
+                        <th class="th">Método de Autenticación</th>
+                    </tr>
+
+                    <tr>
+                        <td>'.$app['conection_file'].'</td>
+                        <td>'.$app['firms'].'</td>
+                        <td>'.$app['attached_files'].'</td>
+                        <td>'.$app['authentication_method'].'</td>
+                    </tr> 
+
+                    <tr>
+                        <th colspan="4" class="th">Descripción</th>
+                    </tr>
+
+                    <tr>
+                        <td colspan="4">'.$app['description'].'</td>
+                    </tr>
+                        
+                </table>';
+
+                
 
             ?>
 
---
+++ View/img/reporte.php
@@ -0,0 +1,136 @@
+
+<br><h3>Datos del Lenguaje:</h3>
+
+  <table border="1" style="font-size:10px; padding:5px 10px;">
+    
+    <tr>
+      <th style="background-color: #354757; color: #fff; text-align:center">Nombre</th>
+    </tr>
+          
+    <tr>
+      <td>$nombreLeng</td>
+    </tr> 
+          
+  </table>
+
+
+
+<br><h3>Datos del Servidor Web:</h3>
+
+  <table border="1" style="font-size:10px; padding:5px 10px;">
+    
+    <tr>
+      <th style="background-color: #354757; color: #fff; text-align:center">Nombre</th>
+    </tr>
+          
+    <tr>
+      <td>$serviweb</td>
+    </tr> 
+          
+  </table>
+
+
+<br><h3>Datos del Sistema Operativo:</h3>
+
+<table border="1" style="font-size:10px; padding:5px 10px;">
+  
+  <tr>
+    <th style="background-color: #354757; color: #fff; text-align:center">Nombre</th>
+  </tr>
+        
+  <tr>
+    <td>$so</td>
+  </tr> 
+        
+</table>
+
+<br><br><br>
+<img src="images/images.png">
+<br><h3>Datos del Ambiente:</h3>
+
+  <table border="1" style="font-size:10px; padding:5px 10px;">
+    
+    <tr>
+      <th style="background-color: #354757; color: #fff; text-align:center">Estatus</th>
+    </tr>
+          
+    <tr>
+      <td>$ambiente</td>
+    </tr> 
+          
+  </table>
+
+<br><h3>Datos del Dispositivo:</h3>
+
+  <table border="1" style="font-size:10px; padding:5px 10px;">
+    
+    <tr>
+      <th style="background-color: #354757; color: #fff; text-align:center">Descripción</th>
+    </tr>
+          
+    <tr>
+      <td>$nombreDisp</td>
+    </tr> 
+          
+  </table>
+
+<br><h3>Datos de la Base de Datos:</h3>
+
+  <table border="1" style="font-size:10px; padding:5px 10px;">
+    
+    <tr>
+      <th style="background-color: #354757; color: #fff; text-align:center">Nombre</th>
+      <th style="background-color: #354757; color: #fff; text-align:center">Estatus</th>
+      <th style="background-color: #354757; color: #fff; text-align:center">Servidor BD</th>
+      <th style="background-color: #354757; color: #fff; text-align:center">Descripción</th>
+    </tr>
+          
+    <tr>
+      <td>$database</td>
+      <td>$so</td>
+      <td>$so</td>
+      <td>$so</td>
+    </tr> 
+          
+  </table>
+
+<br><h3>Datos del Proveedor:</h3>
+
+  <table border="1" style="font-size:10px; padding:5px 10px;">
+    
+    <tr>
+      <th style="background-color: #354757; color: #fff; text-align:center">Nombre</th>
+      <th style="background-color: #354757; color: #fff; text-align:center">Correo</th>
+      <th style="background-color: #354757; color: #fff; text-align:center">Teléfono</th>
+    </tr>
+          
+    <tr>
+      <td>$proveedor</td>
+      <td>$so</td>
+      <td>$so</td>
+      <td>$so</td>
+    </tr> 
+          
+  </table>
+
+<table>
+     <br><br><br><br><br><br><br><br><br><br>
+     <tr>
+     <td style="height:50px;"></td>
+     </tr>
+ 
+ </table>
+ 
+ <table style="font-size: 12px; padding:5px 10px">
+ 
+     <tr>
+         
+         <td style="background-color:white; width:190px;text-align:center"></td>
+          
+          <td style="border-top: 1px solid black;color: #333; background-color:white; width:160px;text-align:center">Firma y sello del Director de Desarrollo</td>
+          
+          <td style="background-color:white; width:190px;text-align:center"></td>
+     
+     </tr>
+ 
+ </table>
